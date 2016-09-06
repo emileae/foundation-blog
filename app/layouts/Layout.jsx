1c37ef8d62a234acfb0cd88c8a7c219904439b94
Emile <emile.app@gmail.com>
2016-09-06 12:01:56 +0200

    first commit

--- app/layouts/Layout.jsx
+++ app/layouts/Layout.jsx
@@ -45,34 +45,12 @@ export var Layout = React.createClass({
     // };
 
     return (
-      <div className="demo-blog mdl-layout mdl-js-layout has-drawer is-upgraded">
-        <main className="mdl-layout__content">
-
+      <div className="row">
+        <div className="small-12 columns">
+        <main className="content">
             { this.props.children }
-
-          <footer className="mdl-mini-footer">
-            <div className="mdl-mini-footer--left-section">
-              <button className="mdl-mini-footer--social-btn social-btn social-btn__twitter">
-                <span className="visuallyhidden">Twitter</span>
-              </button>
-              <button className="mdl-mini-footer--social-btn social-btn social-btn__blogger">
-                <span className="visuallyhidden">Facebook</span>
-              </button>
-              <button className="mdl-mini-footer--social-btn social-btn social-btn__gplus">
-                <span className="visuallyhidden">Google Plus</span>
-              </button>
-            </div>
-            <div className="mdl-mini-footer--right-section">
-              <button className="mdl-mini-footer--social-btn social-btn__share">
-                <i className="material-icons" role="presentation">share</i>
-                <span className="visuallyhidden">share</span>
-              </button>
-            </div>
-          </footer>
-
         </main>
-
-        <div className="mdl-layout__obfuscator"></div>
+        </div>
       </div>
     )
   }
